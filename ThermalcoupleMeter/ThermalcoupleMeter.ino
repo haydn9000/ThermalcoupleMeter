6e3cc50d1335ca02a3c83fc1088f333cf6bf874e
Noir <haydnady@gmail.com>
2021-05-07 17:40:31 -0400

    Thermalcouple code added

--- ThermalcoupleMeter/ThermalcoupleMeter.ino
+++ ThermalcoupleMeter/ThermalcoupleMeter.ino
@@ -1,22 +1,45 @@
-#include "SSD1306Wire.h"  // legacy include: `#include "SSD1306.h"` https://github.com/ThingPulse/esp8266-oled-ssd1306
+#include <Adafruit_MAX31855.h>
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"` https://github.com/ThingPulse/esp8266-oled-ssd1306
 #include <Arduino.h>
 #include <SPI.h>
 
 using namespace std;
 
-#define     ETH_RST         4
-#define     ETH_CS          5
-#define     ETH_SCLK       18
-#define     ETH_MISO       23
-#define     ETH_MOSI       19
-#define     OLED_SCL       22
-#define     OLED_SDA       21
+// Thermocouple instance with software SPI on these three digital IO pins bellow.
+#define MAXCLK 5
+#define MAXCS 18
+#define MAXDO 19
+
+#define OLED_SDA 21
+#define OLED_SCL 22
+
+// initialize the Thermocouple
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
 // Initialize the OLED display using Wire library
-SSD1306Wire  display(0x3c, OLED_SDA, OLED_SCL);
+SSD1306Wire display(0x3c, OLED_SDA, OLED_SCL);
 
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
+
+  while (!Serial)
+    delay(1); // wait for Serial on Leonardo/Zero, etc
+
+  Serial.println("MAX31855 test");
+  // wait for MAX chip to stabilize
+  delay(500);
+  Serial.print("Initializing sensor...");
+  if (!thermocouple.begin())
+  {
+    Serial.println("ERROR.");
+    while (1)
+      delay(10);
+  }
+  Serial.println("DONE.");
+
+  //  =======================================================
   // put your setup code here, to run once:
   display.init(); // Initialize screen
 
@@ -37,6 +60,26 @@ void setup() {
 }
 
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+
+  // basic readout test, just print the current temp
+  Serial.print("Internal Temp = ");
+  Serial.println(thermocouple.readInternal());
+
+  double c = thermocouple.readCelsius();
+  if (isnan(c))
+  {
+    Serial.println("Something wrong with thermocouple!");
+  }
+  else
+  {
+    Serial.print("C = ");
+    Serial.println(c);
+  }
+  //Serial.print("F = ");
+  //Serial.println(thermocouple.readFahrenheit());
+
+  delay(1000);
 }
