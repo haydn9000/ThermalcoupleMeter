4f98f6e734d1c5121f37d42192424f5ecec5998b
Noir <haydnady@gmail.com>
2021-05-08 08:54:37 -0400

    Updated code (some cleanup)

--- ThermalcoupleMeter/ThermalcoupleMeter.ino
+++ ThermalcoupleMeter/ThermalcoupleMeter.ino
@@ -14,7 +14,7 @@ using namespace std;
 #define OLED_SDA 21
 #define OLED_SCL 22
 
-// initialize the Thermocouple
+// Initialize the Thermocouple
 Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
 // Initialize the OLED display using Wire library
@@ -40,32 +40,19 @@ void setup()
   }
   Serial.println("DONE.");
 
-  //  =======================================================
-  // put your setup code here, to run once:
-  display.init(); // Initialize screen
 
+  display.init(); // Initialize screen
   //display.invertDisplay(); // Inverted display mode
 
   display.setContrast(100, 50, 64);
   // Convenience method to access
   // display.setBrightness(15);
-
-  Serial.begin(115200);
-  Serial.println("ESP32 W5500 Start");
-
-  display.setFont(ArialMT_Plain_16); // Font and size
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0, 0, "Hellow World!");
-
-  display.display();
 }
 
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-
-  // basic readout test, just print the current temp
+  // Basic readout test, just print the current temp
   Serial.print("Internal Temp = ");
   Serial.println(thermocouple.readInternal());
 
@@ -73,6 +60,12 @@ void loop()
   if (isnan(c))
   {
     Serial.println("Something wrong with thermocouple!");
+
+    display.clear();
+    display.setFont(ArialMT_Plain_16); // Font and size
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.drawString(64, 32, "ERROR");
+    display.display();
   }
   else
   {
@@ -81,8 +74,8 @@ void loop()
 
     display.clear();
     display.setFont(ArialMT_Plain_16); // Font and size
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.drawString(20, 20, String(c));
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.drawString(64, 32, String(c));
     display.display();
   }
   //Serial.print("F = ");
